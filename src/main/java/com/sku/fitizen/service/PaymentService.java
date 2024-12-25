2db168b68ed52c86ed3ad91206b93c029b91edd6
duswntmd <wnend1010@naver.com>
2024-12-25 17:09:26 +0900

    포인트서비스

--- src/main/java/com/sku/fitizen/service/PaymentService.java
+++ src/main/java/com/sku/fitizen/service/PaymentService.java
@@ -20,7 +20,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -35,7 +34,6 @@ public class PaymentService {
     PaymentMapper paymentMapper;
     @Autowired
     CartMapper cartMapper;
-
 
     private final IamportClient iamportClient;
 
