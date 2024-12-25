a15d80912e0294b1da416ed7e35ed81adf631bbe
duswntmd <wnend1010@naver.com>
2024-12-25 15:22:39 +0900

    장바구니컨트롤러

--- src/main/java/com/sku/fitizen/controller/store/CartController.java
+++ src/main/java/com/sku/fitizen/controller/store/CartController.java
@@ -1,6 +1,5 @@
 package com.sku.fitizen.controller.store;
 
-import com.sku.fitizen.domain.pay.Payment;
 import com.sku.fitizen.domain.store.CartItem;
 import com.sku.fitizen.domain.store.Product;
 import com.sku.fitizen.service.PaymentService;
@@ -10,7 +9,10 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -21,8 +23,10 @@ import java.util.List;
 public class CartController {
     @Autowired
     PaymentService paymentService;
+
     @Autowired
     private CartService cartService;
+
     @Autowired
     private ShopService shopService;
 
