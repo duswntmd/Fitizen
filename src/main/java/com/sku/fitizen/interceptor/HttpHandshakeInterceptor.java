e4c74182771c77a603ed47f81b7258987b416f99
duswntmd <wnend1010@naver.com>
2024-12-25 15:34:38 +0900

    채팅핸드쉐이크

--- src/main/java/com/sku/fitizen/interceptor/HttpHandshakeInterceptor.java
+++ src/main/java/com/sku/fitizen/interceptor/HttpHandshakeInterceptor.java
@@ -1,13 +1,10 @@
 package com.sku.fitizen.interceptor;
+
 import com.sku.fitizen.domain.User;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.security.Principal;
@@ -37,7 +34,6 @@ public class HttpHandshakeInterceptor implements HandshakeInterceptor
             attributes.put("user", user);  // 사용자 정보 전달
             attributes.put("roomId", roomId);
             attributes.put("consultId",consultId);
-
 
         }
         return true;
