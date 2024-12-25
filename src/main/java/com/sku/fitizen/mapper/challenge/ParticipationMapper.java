b545c18d0d30f2b5c5d36cb7b1cb70868a5407af
duswntmd <wnend1010@naver.com>
2024-12-25 16:47:32 +0900

    챌린지스케줄러메퍼

--- src/main/java/com/sku/fitizen/mapper/challenge/ParticipationMapper.java
+++ src/main/java/com/sku/fitizen/mapper/challenge/ParticipationMapper.java
@@ -7,11 +7,9 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface ParticipationMapper {
-
 
   // 한 유저가 특정 챌린지에 참여자인지
   int existUser(Participation parti);
